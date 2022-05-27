df46e58df6eb21208a661d2799a86b1a8caed690
Tejaal M <75902517+ENG19CS0334-TEJAAL-M@users.noreply.github.com>
2022-05-27 07:51:20 +0530

    Update RSA.java

--- RSA.java
+++ RSA.java
@@ -1,28 +1,19 @@
-// Java Program to Implement the RSA Algorithm
 import java.math.*;
- 
 class RSA {
     public static void main(String args[])
     {
         int p, q, n, z, d = 0, e, i;
- 
-        // The number to be encrypted and decrypted
         int msg = 12;
         double c;
         BigInteger msgback;
  
-        // 1st prime number p
         p = 3;
- 
-        // 2nd prime number q
         q = 11;
         n = p * q;
         z = (p - 1) * (q - 1);
         System.out.println("the value of z = " + z);
  
         for (e = 2; e < z; e++) {
- 
-            // e is for public key exponent
             if (gcd(e, z) == 1) {
                 break;
             }
@@ -40,17 +31,11 @@ class RSA {
         System.out.println("the value of d = " + d);
         c = (Math.pow(msg, e)) % n;
         System.out.println("Encrypted message is : " + c);
- 
-        // converting int value of n to BigInteger
         BigInteger N = BigInteger.valueOf(n);
- 
-        // converting float value of c to BigInteger
         BigInteger C = BigDecimal.valueOf(c).toBigInteger();
         msgback = (C.pow(d)).mod(N);
-        System.out.println("Decrypted message is : "
-                           + msgback);
-    }
- 
+        System.out.println("Decrypted message is : "+ msgback);
+    } 
     static int gcd(int e, int z)
     {
         if (e == 0)
